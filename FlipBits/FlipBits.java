c48e24f74f3021f99a4886c94ee9066e488eba01
Pranay Sharma R <rps2579@Pranays-MacBook-Air.local>
2021-11-30 22:58:33 +0530

    FlipBits | ADD | funtions to calc consec 1s and 0s arrays

--- FlipBits/FlipBits.java
+++ FlipBits/FlipBits.java
@@ -3,37 +3,102 @@ package FlipBits;
 import java.util.*;
 
 public class FlipBits {
-     public static void main(String[] args) {
+
+    static int n;
+    static int[] arr;
+
+    static int consecutive_ones_count = 0;
+    static int consecutive_zeros_count = 0;
+    static boolean isPreviousElement1 = false;
+
+    static List<Integer> zeros_count = new ArrayList<>();
+    static List<Integer> ones_count = new ArrayList<>();
+    public static void main(String[] args) {
+
+        // getInput();
+        initTestInput();
+
+        initConsecCountLists();
+
+        calculateMaxOneBitsPossible();
+        
+        printOutput();
+    }
+
+    private static void calculateMaxOneBitsPossible() {
+    }
+
+    private static void printOutput() {
+        System.out.print("\nOnes:\t");
+        printList(ones_count);
+        System.out.print("\nZeroes:\t");
+        printList(zeros_count);
+        System.out.println();
+    }
+
+    private static void initConsecCountLists() {
+        if (arr[0] == 1) {
+            consecutive_ones_count += 1;
+            isPreviousElement1 = true;
+        } else
+            consecutive_zeros_count += 1;
+
+        for (int i = 1; i < n; i++) {
+            performCheckForBit(arr[i]);
+        }
+
+        zeros_count.add(consecutive_zeros_count);
+        ones_count.add(consecutive_ones_count);
+    }
+
+    private static void initTestInput() {
+        n = 8;
+        arr = new int[]{ 1,0,0,1,0,0,0,1};
+    }
+
+    private static void getInput() {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] arr = new int[n];
-        int count=0;
-        int count1=0;
-        int result=0;
-        for(int i=0;i<n;i++)
-        {
-            arr[i]=sc.nextInt();
+        n = sc.nextInt();
+        arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
         }
-        for(int i=0;i<n;i++)
-        {
-            if(arr[i]==1 ) 
-                count=0;
-            else
-            {
-                count++;
-                result = Math.max(result, count);
-            }
-                
-        }
-        for(int i=0;i<n;i++)
-        {
-            if(arr[i]==1)
-            {
-                count1++;
-            }
-        }
-        System.out.println(result+count1);
 
         sc.close();
+    }
+
+    private static void performCheckForBit(int num) {
+        if (num == 1) {
+            if (isPreviousElement1) {
+                consecutive_ones_count += 1;
+            } else {
+                isPreviousElement1 = true;
+
+                zeros_count.add(consecutive_zeros_count);
+                ones_count.add(0);
+
+                consecutive_ones_count += 1;
+                consecutive_zeros_count = 0;
+            }
+        } else {
+            if (isPreviousElement1) {
+                isPreviousElement1 = false;
+
+                zeros_count.add(0);
+                ones_count.add(consecutive_ones_count);
+
+                consecutive_ones_count = 0;
+                consecutive_zeros_count += 1;
+            } else {
+                consecutive_zeros_count += 1;
+            }
+        }
+    }
+
+    private static void printList(List<Integer> list) {
+        for (Integer i : list) {
+            System.out.print(i + ", ");
+        }
     }
 }
