808cf40d9a9b1a9ee2f01e19e4de5ec304feb18b
Pranay Sharma R <rps2579@Pranays-MacBook-Air.local>
2021-12-03 22:10:17 +0530

    AOC | Day3 | P1

---
+++ AdventOfCode/Solutions/Day3_P1.java
@@ -0,0 +1,81 @@
+package AdventOfCode.Solutions;
+
+import java.util.Scanner;
+
+public class Day3_P1 {
+    private static int[] onesCount;
+    private static int[] epsilon;
+    private static int[] gamma;
+    private static int totalInputs = 0;
+
+    public static void main(String[] args) {
+        takeInput();
+
+        findGammaAndEpsilonBinaryNumbers();
+
+        System.out.println("Answer: "+ convertToDecimal(gamma) * convertToDecimal(epsilon));
+    }
+
+    private static long convertToDecimal(int[] binaryNumArray) {
+        long decimal = (long) 0;
+        for (int i = binaryNumArray.length-1; i >= 0; i--) {
+              // 1 0 0 1 0
+              // 0 1 2 3 4 
+              //       5-i+1
+                int temp = binaryNumArray[i]; 
+                // System.out.println("i:"+i+", bit:"+temp+", Math.pow:"+Math.pow(2, binaryNumArray.length-i+1));
+                decimal += (long) (temp*Math.pow(2, binaryNumArray.length-i-1));
+        }  
+        // System.out.println("converted :");
+        // printArray(binaryNumArray);
+        // System.out.println("\t"+decimal);
+        return decimal;
+    }
+
+    private static void findGammaAndEpsilonBinaryNumbers() {
+        for (int i = 0; i < onesCount.length; i++) {
+            int currMost = findMostFreqForCurrBit(onesCount[i]);
+            gamma[i] = currMost;
+            epsilon[i] = (currMost==1) ? 0:1;
+        }
+        // printArray(gamma);
+        // printArray(epsilon);
+    }
+
+    private static void printArray(int[] arr) {
+        for (int i : arr) {
+            System.out.print(i + ", ");
+        }
+        System.out.println();
+    }
+
+    private static int findMostFreqForCurrBit(int currOneCount) {
+        return (currOneCount < totalInputs/2) ? 0 : 1;
+    }
+
+    private static void takeInput() {
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNext()) {
+            String input = sc.next();
+
+            if (input.equalsIgnoreCase("exit"))
+                break;
+
+            totalInputs += 1;
+            if (onesCount == null) {
+                onesCount = new int[input.length()];
+                gamma = new int[input.length()];
+                epsilon = new int[input.length()];
+            }
+
+            for (int i = 0; i < input.length(); i++) {
+                onesCount[i] += (input.charAt(i)=='1') ? 1 : 0;
+            }
+        }
+
+        sc.close();
+    }
+
+
+}
